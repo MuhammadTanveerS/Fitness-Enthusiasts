b671d68ec9a4dad6d93a149b05de8c13eda4bb1d
Muhammd Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-03-11 10:49:37 +0500

    Added Splash Screen

--- app/src/main/java/com/example/fitnessenthusiasts/MainActivity.java
+++ app/src/main/java/com/example/fitnessenthusiasts/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.fitnessenthusiasts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fitnessenthusiasts.activities.testMainActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 5000;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.botton_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView3);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               // Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, testMainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIME);
+
     }
 }
---
+++ app/src/main/java/com/example/fitnessenthusiasts/activities/testMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitnessenthusiasts.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.fitnessenthusiasts.R.layout.activity_test_main);
+    }
+}
