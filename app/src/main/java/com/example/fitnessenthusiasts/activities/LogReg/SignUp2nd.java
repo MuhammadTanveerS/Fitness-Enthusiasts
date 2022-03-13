73b6c35bf69a4695966d28beea11e8451029737d
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-03-13 19:55:56 +0500

    Added all registration screens and some tweaking in existing screens

--- app/src/main/java/com/example/fitnessenthusiasts/activities/LogReg/SignUp2nd.java
+++ app/src/main/java/com/example/fitnessenthusiasts/activities/LogReg/SignUp2nd.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
@@ -14,54 +13,40 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.fitnessenthusiasts.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUp extends AppCompatActivity {
-    //Continue Later not Working now
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
-
+public class SignUp2nd extends AppCompatActivity {
 
     //Variables
     ImageView backBtn;
-    Button next, login;
+    Button next;
     TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(com.example.fitnessenthusiasts.R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_up2nd);
 
         //Hooks
         backBtn = findViewById(R.id.signup_back_button);
         next = findViewById(R.id.signup_next_button);
-        login = findViewById(R.id.signup_login_button);
         titleText = findViewById(R.id.title_text);
-
-    }
-
-    public void check(View view) {
-        rootNode = FirebaseDatabase.getInstance();
-        reference = rootNode.getReference("test");
-        reference.setValue("Hi");
     }
 
     public void callNextSignupScreen(View view) {
 
-        Intent intent = new Intent(getApplicationContext(), SignUp2nd.class);
+        Intent intent = new Intent(getApplicationContext(), SignUp3rd.class);
 
         //Add Transition
-        Pair[] pairs = new Pair[4];
+        Pair[] pairs = new Pair[3];
 
         pairs[0] = new Pair<View, String>(backBtn, "backarrow_tran");
         pairs[1] = new Pair<View, String>(next, "next_tran");
-        pairs[2] = new Pair<View, String>(login, "login_signup_tran");
-        pairs[3] = new Pair<View, String>(titleText, "title_tran");
+        pairs[2] = new Pair<View, String>(titleText, "title_tran");
 
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp.this, pairs);
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp2nd.this, pairs);
         startActivity(intent, options.toBundle());
 
     }
+
 }
--- app/src/main/java/com/example/fitnessenthusiasts/activities/LogReg/StartUpScreen.java
+++ app/src/main/java/com/example/fitnessenthusiasts/activities/LogReg/StartUpScreen.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,37 +17,38 @@ import com.example.fitnessenthusiasts.R;
 public class StartUpScreen extends AppCompatActivity {
 
     //Variables
-    ImageView image;
-    TextView slogan;
+//    ImageView image;
+//    TextView slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_up_screen);
 
         //Hooks
-        image = findViewById(R.id.topImage);
-        slogan = findViewById(R.id.slogan_name);
+//        image = findViewById(R.id.topImage);
+//        slogan = findViewById(R.id.slogan_name);
 
     }
 
     public void login(View view){
-        Intent intent = new Intent(StartUpScreen.this, Login.class);
+        Intent intent = new Intent(getApplicationContext(), Login.class);
 
-        Pair[] pairs = new Pair[2];
-        pairs[0] = new Pair<View,String>(image,"logo_image");
-        pairs[1] = new Pair<View,String>(slogan,"logo_text");
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View,String>(findViewById(R.id.btn_login),"transition_login");
+        //pairs[1] = new Pair<View,String>(slogan,"logo_text");
 
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(StartUpScreen.this,pairs);
         startActivity(intent,options.toBundle());
     }
 
     public void signup(View view){
-        Intent intent = new Intent(StartUpScreen.this, SignUp.class);
+        Intent intent = new Intent(getApplicationContext(), SignUp.class);
 
-        Pair[] pairs = new Pair[2];
-        pairs[0] = new Pair<View,String>(image,"logo_image");
-        pairs[1] = new Pair<View,String>(slogan,"logo_text");
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View,String>(findViewById(R.id.btn_signup),"transition_signup");
+//        pairs[1] = new Pair<View,String>(slogan,"logo_text");
 
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(StartUpScreen.this,pairs);
         startActivity(intent,options.toBundle());
