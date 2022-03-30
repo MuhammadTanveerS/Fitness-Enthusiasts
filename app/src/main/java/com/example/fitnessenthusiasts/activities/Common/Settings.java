1075900292a9055a0fd9509430d9fe8a7e142ca0
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-03-30 17:38:04 +0500

    added proifle and homescreen screens and some modificaiton to existing screens

--- app/src/main/java/com/example/fitnessenthusiasts/activities/Common/Settings.java
+++ app/src/main/java/com/example/fitnessenthusiasts/activities/Common/Settings.java
@@ -1,18 +1,44 @@
 package com.example.fitnessenthusiasts.activities.Common;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.example.fitnessenthusiasts.R;
+import com.example.fitnessenthusiasts.databinding.ActivitySettingsBinding;
 
 public class Settings extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
+    ActivityResultLauncher<String> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_settings);
+        setContentView(binding.getRoot());
+
+        launcher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                binding.settingCircleImageView.setImageURI(result);
+            }
+        });
+
+        binding.changeProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launcher.launch("image/*");
+            }
+        });
+
     }
 }
