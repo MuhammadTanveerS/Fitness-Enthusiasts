6179a74750257514ba4848852b4f11fef92e5aae
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-09-10 08:26:28 +0500

    added more exercises

--- app/src/main/java/com/example/fitnessenthusiasts/activities/HelperClasses/Exercises.java
+++ app/src/main/java/com/example/fitnessenthusiasts/activities/HelperClasses/Exercises.java
@@ -10,6 +10,11 @@ public class Exercises {
     static int jumping_squats = R.raw.jumping_squats;
     static int cobra_stretches = R.raw.cobrastretch;
     static int punches = R.raw.punches;
+    static int staggered_push = R.raw.staggered_push;
+    static int frogpress = R.raw.frogpress;
+    static int pushup = R.raw.pushup;
+    static int seatedabscircle = R.raw.seatedabscircle;
+    static int squatreach = R.raw.squatreach;
 
 
     public static int ex(String name){
@@ -27,6 +32,21 @@ public class Exercises {
                 break;
             case "Punches":
                 exercise = punches;
+                break;
+            case "Staggered PushUp":
+                exercise = staggered_push;
+                break;
+            case "Frog Press":
+                exercise = frogpress;
+                break;
+            case "PushUp":
+                exercise = pushup;
+                break;
+            case "Seated Abs Circle":
+                exercise = seatedabscircle;
+                break;
+            case "Squat Reach":
+                exercise = squatreach;
                 break;
         }
 
